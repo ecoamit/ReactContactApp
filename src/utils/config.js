357c8fb66ab03d4c1508fede05b602c5afcd7b19
unknown <ajamit24singh@gmail.com>
2020-04-19 23:25:24 +0530

    initial commit

---
+++ src/utils/config.js
@@ -0,0 +1,21 @@
+//TODO: add firebase configuration and export it
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+
+ // Your web app's Firebase configuration
+ export var firebaseConfig = {
+  apiKey: "AIzaSyCf1HGS8GTQ89hDxzLZYwIVAlLb9nnBqQQ",
+  authDomain: "contactapp-185a2.firebaseapp.com",
+  databaseURL: "https://contactapp-185a2.firebaseio.com",
+  projectId: "contactapp-185a2",
+  storageBucket: "contactapp-185a2.appspot.com",
+  messagingSenderId: "890703378415",
+  appId: "1:890703378415:web:6468c3bc39e09347d8681a",
+  measurementId: "G-9YX54R7SV9"
+};
